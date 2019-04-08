b7ac69140d2fbabc2b870874dcc872304d11b8b9
Tanner Thurman <robert.thurman@drake.edu>
2019-04-08 08:57:04 -0500

    initial commit

---
+++ DA Capstone Project 2.R
@@ -0,0 +1,7 @@
+library(sqldf)
+
+brief_sum <- read.csv("/Users/tannerthurman/Desktop/DA Capstone 2/brief_summaries.csv", header = T)
+designs <- read.csv("/Users/tannerthurman/Desktop/DA Capstone 2/designs.csv", header = T)
+interventions <- read.csv("/Users/tannerthurman/Desktop/DA Capstone 2/interventions.csv", header = T)
+studies <- read.csv("/Users/tannerthurman/Desktop/DA Capstone 2/studies.csv", header = T)
+
