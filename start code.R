275f63d12427416d7c40a8796bff9b0b739ed35d
Claire Peterson <claire.peterson@drake.edu>
2019-04-15 09:09:53 -0500

    added data cleaning stuff

--- start code.R
+++ start code.R
@@ -1,15 +1,49 @@
-#read data
-studies <- read.csv('/Users/ClairePeterson5/Desktop/YEAR THREE/Spring 2019/STAT 190/Project 2/studies.csv')
-brief_summaries <- read.csv('/Users/ClairePeterson5/Desktop/YEAR THREE/Spring 2019/STAT 190/Project 2/brief_summaries.csv')
-designs <- read.csv('/Users/ClairePeterson5/Desktop/YEAR THREE/Spring 2019/STAT 190/Project 2/designs.csv')
-interventions <- read.csv('/Users/ClairePeterson5/Desktop/YEAR THREE/Spring 2019/STAT 190/Project 2/interventions.csv')
+library(ggplot2) #for plotting
+library(RColorBrewer) #for custom color palettes
+library(dplyr) #for manipulating, aggregating, piping
+library(tidytext) #for (tidy) text mining
+library(topicmodels) #for LDA (topic modeling)
+library(reshape2) #for reshaping data (long to wide or wide to long)
+
+#read data in
+brief_summaries=read.csv(file.choose(), header = TRUE)
+designs=read.csv(file.choose(),header = TRUE)
+interventions=read.csv(file.choose(), header = TRUE)
+studies=read.csv(file.choose(), header = TRUE)
+
+
+str(brief_summaries)
+str(designs)
+str(interventions)
+str(studies)
+
+
+brief_summaries$nct_id=as.character(brief_summaries$nct_id)
+brief_summaries$description=as.character(brief_summaries$description)
+
+
+designs$nct_id=as.character(designs$nct_id)
+designs$allocation=as.character(designs$allocation)
+designs$primary_purpose=as.character(designs$primary_purpose)
+designs$intervention_model=as.character(designs$intervention_model)
+
+
+interventions$nct_id=as.character(interventions$nct_id)
+interventions$x=as.character(interventions$x)
+
+
+studies$nct_id=as.character(studies$nct_id)
+studies$overall_status=as.character(studies$overall_status)
+studies$brief_title=as.character(studies$brief_title)
+studies$start_dt=as.Date(studies$start_dt)
+studies$startMonth=as.numeric(as.POSIXlt(studies$start_dt)$mon+1)
+studies$startYear=as.numeric(as.POSIXlt(studies$start_dt)$year+1900)
 
 # summary of intervention type
 summary(interventions$x)
 
 # summary of trial status
 summary(studies$overall_status)
-
 
 #merging studies and brief summaries
 study_summary <- merge(studies, brief_summaries, by = 'nct_id', all.x = T, all.y = T)
@@ -34,14 +68,9 @@ hist(study_summary_designs_interventions$enrollment, breaks = )
 current_data$status_bin <- 1 #setting it always equal to 1
 current_data$status_bin[current_data$overall_status == 'Completed'] <- 0 #when trial was completed, set to 0
 
-
-m1 <- lm(status_bin ~ phase + enrollment + has_dmc +  allocation + primary_purpose + intervention_model + intervention_type, 
-         data = current_data)
-summary(m1)
-
-
 temp <- current_data[which(current_data$enrollment >=1000 & current_data$enrollment < Inf ),]
 hist(temp$enrollment)
+
 # 1st: 0-21,     26,899
 # 2nd: 22-43,    27,459
 # 3rd: 44-80,    26,049
@@ -51,4 +80,3 @@ hist(temp$enrollment)
 
 # create levels of enrollment
 current_data$enrollment_level <- cut(current_data$enrollment, c(-1,21,43,80,199,999,67128927))
-
