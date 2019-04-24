33e080a4b54fae4908a81145202ffe76ad53ab24
Claire Peterson <claire.peterson@drake.edu>
2019-04-24 08:58:42 -0500

    DATA CLEANING

--- start code.R
+++ start code.R
@@ -183,3 +183,13 @@ ggplot(data = current_data) + geom_mosaic(aes(x = product(overall_status, interv
   labs(x = 'Intervention Type', y = 'Overall Status', fill = 'Overall Status') + ggtitle("Intervention Type and Overall Status") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+## clean allocation
+current_data$allocation[current_data$allocation == 'Random Sample'] <- 'Randomized' #combining random sample with randomized
+
+## rename blank values in allocation to "Not Listed"
+current_data$allocation[current_data$allocation == ''] <- 'Not Listed' 
+## rename blank values in intervention_model to "Not Listed"
+current_data$intervention_model[current_data$intervention_model == ''] <- 'Not Listed' 
+## rename NA values in intervention_type to 'Not Listed'
+current_data$intervention_type[is.na(current_data$intervention_type)] <- 'Not Listed' 
+
