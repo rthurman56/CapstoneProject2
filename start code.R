3bd09de93f41fd9310a9b69cc164b180ca451375
KSchulz78 <kaleb.schulz@drake.edu>
2019-04-24 09:01:53 -0500

    Created Phase Cleaning

--- start code.R
+++ start code.R
@@ -183,3 +183,8 @@ ggplot(data = current_data) + geom_mosaic(aes(x = product(overall_status, interv
   labs(x = 'Intervention Type', y = 'Overall Status', fill = 'Overall Status') + ggtitle("Intervention Type and Overall Status") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+#fix phases
+current_data$phase[current_data$phase=='Early Phase 1']='Phase 1'
+current_data$phase[current_data$phase=='Phase 1/Phase 2']='Phase 1'
+current_data$phase[current_data$phase=='Phase 2/Phase 3']='Phase 2'
+
