aef7cefe0700a99a82e17f80a7555491a174ffbf
Tanner Thurman <robert.thurman@drake.edu>
2019-05-08 19:50:59 -0500

    removed unnecessary file

--- DA Capstone Project 2.R
+++
@@ -1,8 +0,0 @@
-library(sqldf)
-
-brief_sum <- read.csv("/Users/tannerthurman/Desktop/DA Capstone 2/brief_summaries.csv", header = T)
-designs <- read.csv("/Users/tannerthurman/Desktop/DA Capstone 2/designs.csv", header = T)
-interventions <- read.csv("/Users/tannerthurman/Desktop/DA Capstone 2/interventions.csv", header = T)
-studies <- read.csv("/Users/tannerthurman/Desktop/DA Capstone 2/studies.csv", header = T)
-
-summary(studies)
